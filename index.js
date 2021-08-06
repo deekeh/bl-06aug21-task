9499175a2f702312ccbb8110480e3490e215d348
DK <darshankhot@programmer.net>
2021-08-06 12:45:34 +0530

    added uc2

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ function task1(message = 'Hello World') {
   return reversedWords.join(' ');
 }
 
-console.log(task1());
+// 2. Print numbers from 1 to 10
+function task2(start=1, end=10) {
+  let numbers = [];
+  for (let i=start; i<=end; i++) numbers.push(i);
+  return numbers.join(", ");
+}
+
+console.log(task2());
